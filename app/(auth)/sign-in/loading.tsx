405d7e197ca8861ff3f0a4bf3f3beb22d772806d
Mayank-Radadiya <mayankradadiya2@gmail.com>
2025-05-10 22:50:12 +0530

    ADD: Loader

---
+++ app/(auth)/sign-in/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "@/components/custom/Loader";
+
+export default function Loading() {
+  return (
+    <>
+      <Loader />
+    </>
+  );
+}
---
+++ components/custom/Loader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Loader() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="relative animate-pulse">
+        <Image
+          src="/Github.svg"
+          alt="GitVision Logo"
+          width={60}
+          height={60}
+          className="animate-bounce"
+          style={{
+            animationDuration: "2s",
+            filter: "drop-shadow(0 0 8px rgba(59, 130, 246, 0.5))",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
